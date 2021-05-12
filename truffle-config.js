975b0090dd81dcc7ee263e9705a18b5fb2a673fb
Guceri <guceri@gmail.com>
2021-05-12 13:35:14 -0400

    init

---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+require('babel-register');
+require('babel-polyfill');
+require('dotenv').config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.MNEMONIC;
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 4,       
+      gas: 9500000,        
+      confirmations: 0,    
+      timeoutBlocks: 200,  
+      skipDryRun: true     
+    },
+  },
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      version: "0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: 'istanbul' //berlin is experimental
+    }
+  }
+}
+
