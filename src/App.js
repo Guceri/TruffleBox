09be37aaed83a4f62a144785a0244ef33b31316b
Guceri <guceri@gmail.com>
2021-05-13 15:36:33 -0400

    simplify

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ import './App.css';
 class App extends Component {
 
   async componentDidMount() {
-    await this.loadWeb3()
-  }
-
-  async loadWeb3() {
-
+    
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum) 
     }else {
