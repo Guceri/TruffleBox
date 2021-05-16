dce2d9ed4f74754ea7c496f5e62143720e1da338
Guceri <guceri@gmail.com>
2021-05-16 15:17:36 -0400

    notes

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,5 @@
+//use only the contract name, not the source file name
+//one source file could have multiple contracts
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
