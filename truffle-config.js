b0e8b514210bca9570d8034638d6663a364e0f0f
Guceri <guceri@gmail.com>
2021-06-18 12:58:12 -0400

    notes

--- truffle-config.js
+++ truffle-config.js
@@ -19,7 +19,7 @@ module.exports = {
       timeoutBlocks: 200,  
       skipDryRun: true     
     },
-    matic: {
+    matic: { //Not yet tested
       provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
       network_id: 80001,
       confirmations: 2,
